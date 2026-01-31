69e25c4fcebdfad30cf197ee205db9e83eff786b
AldairOM <aldairolaya01@gmail.com>
2026-01-30 23:55:55 -0600

    Change email transporter to use Brevo SMTP

--- server.js
+++ server.js
@@ -8,15 +8,12 @@ app.use(express.json({ limit: '10mb' }));
 
 //Se configura el gmail para el envío de emails
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS
   }
 });
 
